bcbd5451af16dd9fd58f79232001e501a28797a9
fredisonrojas <fredisonrojas.developer@gmail.com>
2024-10-19 17:32:31 -0500

    ya quedo realizado el conversor de moneda

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,59 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner lectura = new Scanner(System.in);
+        ConsultarMoneda consulta = new ConsultarMoneda();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int opcion=0;
+        while (opcion != 8){
+            System.out.println("****************************\n" +
+                    "Bienvenidos al Conversor de monedas\n\n"+
+                    "Ingrese la conversion que desea realizar\n\n"+
+                    "1. Dollar a peso Argentino\n"+
+                    "2. Peso Argentino A Dollar\n"+
+                    "3. Dollar a Real Brasilero¸\n"+
+                    "4. Real Brasilero a Dollar\n"+
+                    "5. Dollar a Peso Colombiano\n"+
+                    "6. Peso Colombiano a Dollar\n"+
+                    "7. Convertir a otra moneda\n"+
+                    "8. Salir");
+            opcion = lectura.nextInt();
+            lectura.nextLine();
+
+            switch (opcion){
+                case 1:
+                    ConvertirMoneda.convertir( "USD", "ARS",consulta,lectura);
+                    break;
+                case 2:
+                    ConvertirMoneda.convertir( "ARS", "USD",consulta,lectura);
+                    break;
+                case 3:
+                    ConvertirMoneda.convertir( "USD", "BRL",consulta,lectura);
+                    break;
+                case 4:
+                    ConvertirMoneda.convertir( "BRL", "USD",consulta,lectura);
+                    break;
+                case 5:
+                    ConvertirMoneda.convertir( "USD", "COP",consulta,lectura);
+                    break;
+                case 6:
+                    ConvertirMoneda.convertir( "COP", "USD",consulta,lectura);
+                    break;
+                case 7:
+                    ConvertirMoneda.convertirOtraMoneda(consulta,lectura);
+                    break;
+                case 8:
+                    System.out.println("saliendo...");
+                default:
+                    System.out.println("opcion no validad");
+                    break;
+            }
         }
+
     }
 }
